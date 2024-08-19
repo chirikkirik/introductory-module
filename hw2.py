5aa4ba5b1c4d3611e24788c123df1a8a1e3273c2
chirikkirik <149368902+chirikkirik@users.noreply.github.com>
2024-08-19 23:03:51 +0300

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,23 @@
+#1st program
+from time import process_time
+
+print(9**0.5*5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+#4th program
+a = '123.456'
+b = float(a)
+b = b * 10
+c = int(b)
+c = c % 10
+print(c)
+
+
+
